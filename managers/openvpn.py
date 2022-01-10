f6bb436ddc10db58cd4cb333bced7b9e6f69609b
david.moya <david.moya@invisiblebits.com>
2022-01-10 22:48:16 +0100

    Create modules

---
+++ managers/openvpn.py
@@ -0,0 +1,65 @@
+from subprocess import run, Popen, PIPE
+import os
+import time
+
+
+class OpenVpn(object):
+
+    HOME = os.environ['HOME']
+    config_file = HOME + "/Documents/pfsense/vpn/david.moya-config.conf"
+    sudo_pass = ''
+    file_path = "/tmp/.sudo_pass.txt"
+
+    def __init__(self):
+        if os.path.isfile(self.file_path) and self.time_live():
+            file = open(self.file_path, "r")
+            self.sudo_pass = file.read()
+            file.close()
+        else:
+            password = input("add sudo password:")
+            self.sudo_pass = password
+            file = open(self.file_path, "w")
+            file.write(self.sudo_pass)
+            file.close()
+    
+    def status(self):
+        pid = Popen(["pidof", "openvpn"], stdout=PIPE)
+        output, error = pid.communicate()
+        if output:
+            return "UP"
+        else:
+            return "DOWN"
+
+    def time_live(self):
+        now = time.time()
+        live = now - 86400
+        time_creation_file = os.path.getmtime(self.file_path)
+        if time_creation_file > live:
+            return True
+        else:
+            return False
+    
+    def delete_password(self):
+        try:
+            os.remove(self.file_path)
+        except OSError as e:
+            print("Error: %s : %s" % (self.file_path, e.strerror))
+
+    def up(self):
+        subprocess = Popen(["sudo", "-S", "openvpn", "--route-gateway", "dhcp", "--allow-pull-fqdn", "--config", self.config_file], stderr=PIPE, stdin=PIPE, universal_newlines=True)
+        out, err = subprocess.communicate(self.sudo_pass + '\n')
+        if 'incorrect' in err: 
+            self.delete_password()
+            return "ERROR"
+        return "UP"
+
+    def down(self):
+        pid = Popen(["pidof", "openvpn"], stdout=PIPE)
+        output, error = pid.communicate()
+        PID = output.split(None, 1)[0]
+        subprocess = Popen(["sudo", "-S", "kill", "-9", PID], stderr=PIPE, stdin=PIPE, universal_newlines=True)
+        out, err = subprocess.communicate(self.sudo_pass + '\n')
+        if 'incorrect' in err: 
+            self.delete_password()
+            return "ERROR"
+        return "DOWN"
